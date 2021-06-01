a7419be18cb13d906238f6902c6ce3e38168fe61
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2021-06-01 12:35:05 +0600

    Компьютерные курсы
    Залил проект

---
+++ computerCourses/js/main.js
@@ -0,0 +1,13 @@
+// const  countDownEl = document.getElementById("countdown");
+// var time  = 1555200000;
+// setInterval(updateCountdown, 1000);
+
+// function updateCountdown(){
+//     var days = Math.floor(time / (1000 * 60 * 60 *24));
+//     var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((time % (1000 * 60 )) / 1000);
+//     seconds = seconds < 10 ? '0' + seconds : seconds;
+//     countDownEl.innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+//     time--;
+// }
